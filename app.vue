ec15e30b3c97a75d70548105387940eb4719dfcf
Jacob Long <jclong98@gmail.com>
2022-04-22 22:29:10 -0700

    update button styling

--- app.vue
+++ app.vue
@@ -69,11 +69,25 @@ const fullImagePath = computed(
 
       <div class="absolute inset-x-0 -bottom-8 flex justify-center">
         <button
-          class="text-2xl relative bg-green-500 w-32 h-16 rounded-full overflow-auto cursor-pointer shadow hover:shadow-xl transition duration-200"
+          class="text-2xl relative text-white bg-green-500 w-32 h-16 rounded-full overflow-auto cursor-pointer shadow hover:shadow-xl transition duration-200 flex justify-center items-center active:scale-90"
           @click="imageId = randomLetters(3)"
           title="randomize"
         >
-          🎲
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            aria-hidden="true"
+            role="img"
+            width="32"
+            height="32"
+            preserveAspectRatio="xMidYMid meet"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill="currentColor"
+              d="M3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3H3zm2.5 4a1.5 1.5 0 1 1-3 0a1.5 1.5 0 0 1 3 0zm8 0a1.5 1.5 0 1 1-3 0a1.5 1.5 0 0 1 3 0zM12 13.5a1.5 1.5 0 1 1 0-3a1.5 1.5 0 0 1 0 3zM5.5 12a1.5 1.5 0 1 1-3 0a1.5 1.5 0 0 1 3 0zM8 9.5a1.5 1.5 0 1 1 0-3a1.5 1.5 0 0 1 0 3z"
+            ></path>
+          </svg>
         </button>
       </div>
     </main>
