800d2ca23444f294d7dc7f979c18a7c4e7e35c59
Jacob Long <jclong98@gmail.com>
2022-04-22 18:06:07 -0700

    add vueuse for reactive url query

--- app.vue
+++ app.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+const route = useRoute() // used for page loading -- from nuxt
+const searchParams = useUrlSearchParams() // used for reactive search params in url bar -- from vueuse
+
 const letters = 'abcdefghijklmnopqrstuvwxyz'
 const randomLetter = () => {
   return letters[Math.floor(Math.random() * letters.length)]
@@ -7,7 +10,18 @@ const randomLetters = (length: number) => {
   return Array.from({ length }, () => randomLetter()).join('')
 }
 
-const imageId = ref(randomLetters(3))
+const imageId = computed({
+  get() {
+    // first check if vueuse's search params are set
+    // if not, use the route's params from the server
+    // if that isn't set, use a random string
+    return searchParams.id ?? route.query.id ?? randomLetters(3)
+  },
+  set(id: string) {
+    searchParams.id = id
+  },
+})
+
 const imagePath = computed(() => `/api/${imageId.value}`)
 const fullImagePath = computed(
   () => `https://bird-avatars.netlify.app/api/${imageId.value}`
@@ -24,16 +38,16 @@ const fullImagePath = computed(
     <Meta content="/bird-2.png" property="og:image" />
     <Meta content="#FF7043" data-react-helmet="true" name="theme-color" />
 
-    <Title>Bird Avatar Generator</Title>
+    <Title>{{ imageId }} | Bird Avatar Generator</Title>
 
     <Link rel="icon" href="favicon.svg" type="image/svg+xml" />
   </Head>
 
   <div class="min-h-screen bg-stone-200 grid place-items-center px-4">
     <main
-      class="relative bg-white text-stone-900 p-4 rounded-xl grid gap-4 w-full sm:w-prose relative pb-12 shadow-lg"
+      class="relative bg-white text-stone-900 p-4 rounded-xl grid gap-4 w-full sm:w-prose relative pb-12 shadow-lg mb-8"
     >
-      <header class="text-center text-3xl font-bold">
+      <header class="text-center text-3xl font-bold my-4">
         <h1>Bird Avatar Generator</h1>
       </header>
 
@@ -48,7 +62,7 @@ const fullImagePath = computed(
           class="text-center p-4 rounded-xl w-full sm:w-90 bg-stone-100"
           type="text"
           tabindex="0"
-          @focus="$event.target.select()"
+          @focus="($event.target as HTMLInputElement).select()"
           :value="fullImagePath"
         />
       </div>
@@ -64,13 +78,18 @@ const fullImagePath = computed(
       </div>
     </main>
 
-    <footer class="fixed bottom-1 inset-x-0 text-center text-stone-800">
-      Created by
-      <a
-        class="font-bold text-stone-600"
-        href="https://github.com/jclong98/bird-avatars"
-        >Jacob Long</a
-      >
+    <footer class="sm:fixed bottom-0 right-0 p-2 text-stone-800">
+      <a class="text-stone-600" href="https://github.com/jclong98/bird-avatars">
+        Created by
+
+        <span class="font-bold">Jacob Long</span>
+
+        <img
+          class="inline rounded-full h-8 w-8 mx-2"
+          src="https://avatars.githubusercontent.com/u/36164786?s=48&v=4"
+          alt="my github profile pic"
+        />
+      </a>
     </footer>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  buildModules: ['@unocss/nuxt'],
+  buildModules: ['@unocss/nuxt', '@vueuse/nuxt'],
   unocss: {
     // presets
     preflight: true,
