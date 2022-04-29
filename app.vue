32f0c37900c83cfa7bcdbb46323125d421a0a605
Jacob Long <jclong98@gmail.com>
2022-04-28 17:50:24 -0700

    add preset icons

--- app.vue
+++ app.vue
@@ -117,26 +117,12 @@ const themeColor = ref('blue')
           w="32"
           h="16"
           relative
-          text="2xl white"
+          text="3xl white"
           :class="`bg-${themeColor}-500`"
           @click="imageId = randomLetters(3)"
           title="randomize"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            aria-hidden="true"
-            role="img"
-            width="32"
-            height="32"
-            preserveAspectRatio="xMidYMid meet"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fill="currentColor"
-              d="M3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3H3zm2.5 4a1.5 1.5 0 1 1-3 0a1.5 1.5 0 0 1 3 0zm8 0a1.5 1.5 0 1 1-3 0a1.5 1.5 0 0 1 3 0zM12 13.5a1.5 1.5 0 1 1 0-3a1.5 1.5 0 0 1 0 3zM5.5 12a1.5 1.5 0 1 1-3 0a1.5 1.5 0 0 1 3 0zM8 9.5a1.5 1.5 0 1 1 0-3a1.5 1.5 0 0 1 0 3z"
-            ></path>
-          </svg>
+          <i class="i-bi-dice-5-fill"></i>
         </button>
       </div>
     </main>
